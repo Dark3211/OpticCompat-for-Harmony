5bfa1735a9e9b736c45b20303c9bd7d929c22b89
Dark3211 <44311045+Dark3211@users.noreply.github.com>
2026-08-28 22:13:14 -0600

    Disable runtime log file generation

--- src/log.cpp
+++ src/log.cpp
@@ -1,9 +1,6 @@
 #include "log.hpp"
-#include <cstdarg>
 
 namespace OpticCompat {
-    static std::mutex g_log_mutex;
-
     std::filesystem::path halo_root() {
         std::array<wchar_t, 32768> buffer{};
         const DWORD len = GetModuleFileNameW(nullptr, buffer.data(), static_cast<DWORD>(buffer.size()));
@@ -13,28 +10,6 @@ namespace OpticCompat {
         return std::filesystem::path(buffer.data()).parent_path();
     }
 
-    void log_line(const char *format, ...) {
-        char message[2048]{};
-        va_list args;
-        va_start(args, format);
-        vsnprintf_s(message, sizeof(message), _TRUNCATE, format, args);
-        va_end(args);
-
-        std::lock_guard<std::mutex> lock(g_log_mutex);
-        std::error_code ec;
-        const auto mods = halo_root() / L"mods";
-        std::filesystem::create_directories(mods, ec);
-        const auto path = mods / L"opticcompat.log";
-        std::ofstream out(path, std::ios::app | std::ios::binary);
-        if(!out) {
-            return;
-        }
-
-        SYSTEMTIME st{};
-        GetLocalTime(&st);
-        char stamp[64]{};
-        snprintf(stamp, sizeof(stamp), "[%04u-%02u-%02u %02u:%02u:%02u.%03u] ",
-                 st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
-        out << stamp << message << "\r\n";
+    void log_line(const char *, ...) {
     }
 }
